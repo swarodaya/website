66021cac5b7ab94b8aefb706a9c56c6b3b784644
Tapan Karecha <tapan.karecha@urbanladder.com>
2016-10-24 20:02:24 +0530

    Updated deploy.sh to match my workflow

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,16 @@
+# clean up
+trgt="../public/website"
+rm $trgt/{404.html,favicon.png,index.html,index.xml,logo.png,sitemap.xml}
+rm -r $trgt/{blog,css,img,js}
+
+# pause for input
+read  -n 1 -p "Press key to continue"
+
+# build
 hugo -d ../public/website
-git add -A
-git commit
+
+# commit and push to origin
+git commit -a
 git push origin master
+
 echo "Now go push the generated site!"
